f0c8108467165b701372cdfc90e5f7f2cbdd82b5
Cory Fladhammer <cjfladham@hotmail.com>
2020-01-21 23:11:06 -0600

    new .js file

--- script.js
+++ script.js
@@ -1,62 +1,55 @@
 'use strict';
 
-function getDataFromApi(country) {
+function getDataFromApi(searchTerm, callback) {
 
-    // dynamcially generate the URL
-    
-    const url = `https://restcountries.eu/rest/v2/name/${country}`
+    const url = `https://restcountries.eu/rest/v2/name/${searchTerm}`;
 
-    console.log(`Finding country facts for ${country}`);
+    console.log(url);
 
     fetch(url)
-    .then(response => {
-        if(response.ok) {
-            return response.json();
-        }
-        throw new Error(response.statusText);
-    })
-    .then(responseJson => displaySearchData(responseJson))
-    .catch(err => {
-        $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        $('#js-search-results').empty();
-        $('#js-error-message').removeClass('hidden');
-    });
-
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displaySearchData(responseJson))
+        .catch(err => {
+            console.log(err)
+        });
 }
 
-function displaySearchData(responseJson) {
+function displaySearchData(data) {
 
-    console.log(responseJson);
+    console.log(data[0].name);
 
-    // if there are previous results, remove them
-    $('#js-search-results').html('');
-    $('#js-error-message').empty();
-    console.log('emptied');
+    if (data.length == 0) {
+        alert('no country found')
+    } else {
+        let HtmlOutput = '';
 
-    // adds the country facts
-    $('#js-search-results').append(
-        `<p>Country Name:${responseJson[0].name}</p>
-        <p>Population:${responseJson[0].population}</p>
-        <p>Region:${responseJson[0].region}</p>
-        `);
+        for (let i = 0; i < data.length; i++) {
+            HtmlOutput += '<p>' + data[i].name + '</p></ br> '
+            HtmlOutput += '<p>' + data[i].population + '<p>'
+            HtmlOutput += '<p>' + data[i].capital + '<p>'
+            HtmlOutput += '<p>' + data[i].region + '<p>'
 
-    // displays the results section
-    console.log('country facts displayed');
+            $('.js-search-results').html(HtmlOutput);
+        }
+    }
 
 
-};
+}
 
 function watchSubmit() {
     $('.js-search-form').submit(event => {
         event.preventDefault();
-
-        const country = $('.js-query').val();
-        if (country == '') {
-            alert('please enter country name')
-        }
-        getDataFromApi(country);
+        let queryTarget = $(event.currentTarget).find('.js-query');
+        let query = queryTarget.val();
+        console.log(query);
+        queryTarget.val("");
+        getDataFromApi(query, displaySearchData);
     });
 }
-
 
 $(watchSubmit);
